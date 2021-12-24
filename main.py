439a12d43fc928761123e3870cbd209939b00001
AmirHai <59731951+AmirHai@users.noreply.github.com>
2021-12-24 18:56:05 +0300

    теперь при запуске программы запускается окно главного меню которое имеет размеры 1200 на 800. Этот файл будет отвечать за инициализацию роекта

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('Hello world!!!')
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+
+    scrSize = width, height = 1200, 800
+    screen = pygame.display.set_mode(scrSize)
+    screen.fill((0, 0, 0))
+    pygame.display.set_caption('Soul Knight: Главное меню')
+
+    fps = 60
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        clock.tick(fps)
+
+    pygame.quit()
